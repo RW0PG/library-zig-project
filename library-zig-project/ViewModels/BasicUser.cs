9b04b29b0adfc982885e0ab8c8b157113b502bf3
Kamil Czerwinski <kamilcze31@gmail.com>
2022-01-29 16:05:53 +0100

    Initial IUser interface and implemntations architecture

---
+++ library-zig-project/ViewModels/BasicUser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library_zig_project.ViewModels
+{
+    class BasicUser : IUser
+    {
+        public double amountToBePaid()
+        {
+            throw new NotImplementedException();
+        }
+
+        public AccountType getAccountType()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IBook[] getUserListOfBooksAfterDueDate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IBook[] getUserListOfBorrowedBooks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int numberOfBorrowedBooks()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
